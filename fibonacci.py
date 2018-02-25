fa680af9536e27869e3be546da1311a8df7976ee
Mohamed Abdi <mosuswalks@gmail.com>
2018-02-25 16:13:44 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,5 @@
+
+
 """
 Write a program that asks the user how many Fibonnaci numbers to generate 
 and then generates them. Take this opportunity to think about how you can use functions. 
@@ -7,8 +9,6 @@ Make sure to ask the user to enter the number of numbers in the sequence to gene
 sequence is the sum of the previous two numbers in the sequence.
 The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
 """
-
-
 
 
 def Fibonacci(fibNum):
